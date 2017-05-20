fd64ded289921a03ba74747704f3826d3b5c7183
reficul31 <reficul31@gmail.com>
2017-05-20 11:12:08 +0530

    Adding the snapshot tests for ResultList component

---
+++ __tests__/overview/ResultList.test.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import ResultList from 'src/overview/components/ResultList'
+import { shallow, mount } from 'enzyme'
+import * as search from './search.data.json'
+
+describe('ResultList snapshot tests', () => {
+	test('should compare against the snapshot given', () => {
+		const searchResult = search
+		const searchQuery  = ''
+		const component = shallow(
+			<ResultList searchResult={searchResult} searchQuery={searchQuery} />
+			)
+		expect(component).toMatchSnapshot()
+	})
+
+	test('should compare against the snapshot of no results', () => {
+		const searchResult = {
+  					"rows": []
+				}
+		const searchQuery = ''
+		const component = shallow(
+			<ResultList searchResult={searchResult} searchQuery={searchQuery} />
+		)
+		expect(component).toMatchSnapshot()
+	})
+})
