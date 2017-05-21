38081deffd37518bb5aeb09b4ef4c818acc66848
reficul31 <reficul31@gmail.com>
2017-05-21 11:47:29 +0530

    Adding tests for random-string module

---
+++ __tests__/util/random-string.test.js
@@ -0,0 +1,8 @@
+import randomString from 'src/util/random-string'
+
+describe('randomString tests', () => {
+	test('should return a 10 digit random number', () => {
+        let expected = expect.stringMatching('^[0-9]{10}$')
+        expect(randomString()).toEqual(expected)
+	})
+})
