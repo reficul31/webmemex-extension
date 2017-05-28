4b5e18c64333a7e8deb9d96ed6fdfe18f29fdf22
reficul31 <reficul31@gmail.com>
2017-05-28 09:45:40 +0530

    Adding tests for page-analysis module

---
+++ __tests__/page-analysis/content_script/extract-page-content.test.js
@@ -0,0 +1,26 @@
+import extractPageContent from 'src/page-analysis/content_script/extract-page-content'
+import * as extractPdfContent from 'src/page-analysis/content_script/extract-pdf-content'
+import * as extractPageText from 'src/page-analysis/content_script/extract-page-text'
+jest.mock('page-metadata-parser')
+
+describe('extractPageContent tests', () => {
+	test('should call extractPdfContent if url ends with pdf', async () => {
+		extractPdfContent.default = jest.fn()
+		const pdf = await extractPageContent({
+			loc: 'https://example.com',
+			url: 'https://example.com/doc.pdf',
+			doc: 'document',
+		})
+		expect(extractPdfContent.default).toHaveBeenCalled()
+	})
+
+	test('should call extractPageText if url does not end with pdf', async () => {
+		extractPageText.default = jest.fn()
+		const page = await extractPageContent({
+			loc: 'https://example.com',
+			url: 'https://example.com/blog/post',
+			doc: 'document',
+		})
+		expect(extractPageText.default).toHaveBeenCalled()
+	})
+})
